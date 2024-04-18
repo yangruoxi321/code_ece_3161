687ff81562ef12f34ef0d9f0898e4c0b436f6939
yangruoxi321 <yangruoxi321@gmail.com>
2024-04-17 22:04:16 -0400

    1

---
+++ code.ino
@@ -0,0 +1,23 @@
+const int trigPin = 2;
+const int echoPin = 3;
+long duration;
+float distance;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(500);
+}
